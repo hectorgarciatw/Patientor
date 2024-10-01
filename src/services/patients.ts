f8c3555e39b4f05d1e833bb9c3db029147e25eef
hectorgarciatw <dreamallica@gmail.com>
2024-10-01 14:29:06 -0300

    Exercise 9.11 completed

--- src/services/patients.ts
+++ src/services/patients.ts
@@ -4,23 +4,17 @@ import { Patient, PatientFormValues } from "../types";
 import { apiBaseUrl } from "../constants";
 
 const getAll = async () => {
-  const { data } = await axios.get<Patient[]>(
-    `${apiBaseUrl}/patients`
-  );
-
-  return data;
+    const { data } = await axios.get<Patient[]>(`${apiBaseUrl}/patients`);
+    return data;
 };
 
 const create = async (object: PatientFormValues) => {
-  const { data } = await axios.post<Patient>(
-    `${apiBaseUrl}/patients`,
-    object
-  );
+    const { data } = await axios.post<Patient>(`${apiBaseUrl}/patients`, object);
 
-  return data;
+    return data;
 };
 
 export default {
-  getAll, create
+    getAll,
+    create,
 };
-
